30195c46d298a3552a27ad6748f83cc712702f39
Caleb Adebayo <calebtolorunleke@yahoo.com>
2025-05-05 22:49:56 +0100

    form validation for tech studio

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,80 @@
-import "./App.css";
-import First from "./components/First";
-import Second from "./components/Second";
-import Products from "./components/Products";
+// import "./App.css";
+// import First from "./components/First";
+// import Second from "./components/Second";
+// import Products from "./components/Products";
 
-function App() {
-  return (
-    <div className="flex gap-3">
-      <h1 className="heading">REACT LESSONS</h1>
-      <First />
-      <Second />
-      <Products age={14} />
-      <Products
-        img='https://ng.jumia.is/cms/0-0-black-friday/2024/Thumbnails/home.jpg"'
-        name="philly bol"
-        price={7000}
-      ></Products>
-      <Products
-        img='https://ng.jumia.is/cms/0-0-black-friday/2024/Thumbnails/home.jpg"'
-        name="philly bol"
-        price={7000}
-      />
-      {/* 
-      <Products age={14} />
-      <Products age={14} /> */}
-      <Products
-        img='https://ng.jumia.is/cms/0-0-black-friday/2024/Thumbnails/home.jpg"'
-        name="philly bol"
-        price={7000}
-      >Po<Products />
-    </div>
-  );
-}
+// function App() {
+//   // const customers = ["john", "peter", "ade", "susan"];
 
-export default App;
+//     // <div className="flex gap-3">
+//     //   <h1 className="heading">REACT LESSONS</h1>
+//     //   <p>{customers}</p>
+
+//     //     <div>
+//     //       {
+//     //         customers.map((customer)=>(
+//     //           return (<p>Good</p>)
+//     //         ))
+//     //       }
+//     //     </div>
+//     //   <First />
+//     //   <Second />
+//     //   <Products age={14} />
+//     //   <Products
+//     //     img="https://ng.jumia.is/cms/0-0-black-friday/2024/Thumbnails/home.jpg"
+//     //     name="philly bol"
+//     //     price={7000}
+//     //   ></Products>
+//     //   <Products
+//     //     img="https://ng.jumia.is/cms/0-0-black-friday/2024/Thumbnails/home.jpg"
+//     //     name="philly bol"
+//     //     price={7000}
+//     //   />
+//     //   {/*
+//     //   <Products age={14} />
+//     //   <Products age={14} /> */}{" "}
+//     //   <Products
+//     //     img="https://ng.jumia.is/cms/0-0-black-friday/2024/Thumbnails/home.jpg"
+//     //     name="philly bol"
+//     //     price={7000}
+//     //   ></Products>
+//     // </div>
+
+//     const products = {
+
+//         img="https://ng.jumia.is/cms/0-0-black-friday/2024/Thumbnails/home.jpg",
+//         name = 'OKay John',
+//         price = '500',
+//         id = 1
+
+//     }
+
+//     {
+//       img="https://ng.jumia.is/cms/0-0-black-friday/2024/Thumbnails/home.jpg",
+//       name = 'OKay John',
+//       price = '500',
+//       id = 2
+//     }
+
+//     {
+//       img="https://ng.jumia.is/cms/0-0-black-friday/2024/Thumbnails/home.jpg",
+//       name = 'OKay John',
+//       price = '500',
+//       id = 3
+//     }
+
+//     return (
+//       <div>
+//         <h1 className="heading">REACY LESSONS</h1>
+
+//         <section>
+//           {products.map(product) => {
+//             return <product key = {product.id} {product.img} name={product.name}
+//           }}
+//         </section>
+//       </div>
+//     )
+
+// }
+
+// export default App;
